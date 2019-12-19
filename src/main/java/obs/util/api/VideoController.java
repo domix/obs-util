22e1d8791f631ac9613cbec9c9c045919c37dc01
Domingo Suarez Torres <domingo.suarez@gmail.com>
2019-12-19 13:29:17 -0600

    init support for videos

--- src/main/java/obs/util/api/VideoController.java
+++ src/main/java/obs/util/api/VideoController.java
@@ -2,10 +2,15 @@ package obs.util.api;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.multipart.CompletedFileUpload;
 import obs.util.model.Video;
 import obs.util.service.VideosService;
 
+import java.io.IOException;
 import java.util.List;
+
+import static io.micronaut.http.MediaType.MULTIPART_FORM_DATA;
 
 @Controller("/v1/videos")
 public class VideoController {
@@ -18,5 +23,14 @@ public class VideoController {
   @Get
   public List<Video> list() {
     return videosService.allTasks();
+  }
+
+  @Post(consumes = MULTIPART_FORM_DATA)
+  public String create(CompletedFileUpload video) {
+    try {
+      return video.getFilename() + ": " + video.getBytes().length;
+    } catch (IOException e) {
+      return e.getMessage();
+    }
   }
 }
