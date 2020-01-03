6861dc0b9ab7139e76021c4e89402cba70257880
Domingo Suarez Torres <domingo.suarez@gmail.com>
2020-01-02 18:08:00 -0600

    improved tests

---
+++ src/test/java/obs/util/SmokeTests.java
@@ -0,0 +1,12 @@
+package obs.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SmokeTests {
+  @Test
+  void addition() {
+    assertEquals(2, 2);
+  }
+}
--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-docker build -f Dockerfile_build . && \
-  docker rmi -f $(docker images -q --filter label=stage=builder)
+docker build -f Dockerfile_build .
+exit_code=$?
+
+docker rmi -f $(docker images -q --filter label=stage=builder)
+
+exit "$exit_code"
