ca594c0e8317f18b92c5848598a62a52d421b59d
Domingo Suarez Torres <domingo.suarez@gmail.com>
2020-01-03 13:12:31 -0600

    improved tests

--- src/main/java/obs/util/service/VideosService.java
+++ src/main/java/obs/util/service/VideosService.java
@@ -1,5 +1,7 @@
 package obs.util.service;
 
+import io.reactivex.Maybe;
+import io.reactivex.schedulers.Schedulers;
 import lombok.extern.slf4j.Slf4j;
 import obs.util.model.ActiveVideo;
 import obs.util.model.Resource;
@@ -17,42 +19,77 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.function.Consumer;
 
+import static io.reactivex.Maybe.just;
 import static java.util.Optional.ofNullable;
 
 @Slf4j
 @Singleton
 public class VideosService {
-  private final ConcurrentMap<String, Video> storage;
+  private final ConcurrentMap<String, Video> storage = new ConcurrentHashMap<>();
   private final Yaml yaml;
   private final DateJob dateJob;
   private ActiveVideo activeVideo = new ActiveVideo();
 
   public VideosService(DateJob dateJob) {
     this.dateJob = dateJob;
-    storage = new ConcurrentHashMap<>();
     yaml = new Yaml();
   }
 
-  public List<Video> allTasks() {
-    return new ArrayList<>(storage.values());
-  }
-
-  public Video add(byte[] bytes) {
+  private Video createVideo(byte[] bytes) {
     try (var is = new ByteArrayInputStream(bytes)) {
       Video video = yaml.load(is);
-      log.info(video.getId());
-      storage.put(video.getId(), video);
+      log.info("Loaded video '{}' from bytes.", video.getId());
       return video;
     } catch (Throwable t) {
       throw new RuntimeException(t.getMessage(), t);
     }
   }
 
-  public Video setVideoActive(String id) {
-    Video video = storage.get(id);
-    activeVideo.setResourceIndex(0);
-    activeVideo.setVideo(video);
+  public List<Video> allTasks() {
+    return new ArrayList<>(storage.values());
+  }
+
+  public Maybe<Video> add(byte[] bytes) {
+    log.info("About to add new video from bytes..");
+    return just(bytes)
+      .map(this::createVideo)
+      .map(this::addToStorage);
+  }
+
+  public Video addToStorage(Video video) {
+    storage.put(video.getId(), video);
+    log.info("Video '{}' added to storage.", video.getId());
     return video;
+  }
+
+  public Maybe<Video> findVideoById(final String id) {
+    return just(id)
+      .subscribeOn(Schedulers.io())
+      .map(this::fromStorageOrNull);
+  }
+
+  public Video fromStorageOrNull(String id) {
+    log.info("Trying to find in storage video with id: '{}'", id);
+    Video video = storage.getOrDefault(id, null);
+    log.info("Video found in storage? {}", Objects.nonNull(video));
+    return video;
+  }
+
+  public Optional<Video> findVideo(String id) {
+    return Optional.ofNullable(storage.get(id));
+  }
+
+  public Maybe<Video> setVideoActive(String id) {
+    log.info("About to activate the video '{}'...", id);
+    return Maybe.just(id)
+      .map(this::findVideoById)
+      .map(videoMaybe -> {
+        Video video = videoMaybe.blockingGet();
+        activeVideo.setResourceIndex(0);
+        activeVideo.setVideo(video);
+        log.info("Video '{}' activated.", video.getId());
+        return video;
+      });
   }
 
   public ActiveVideo getActive() {
