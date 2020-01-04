fb5f7a19b4af1af1076799eee53811562dddafc6
Domingo Suarez Torres <domingo.suarez@gmail.com>
2020-01-03 18:28:53 -0600

    containerzation done

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,16 @@
-#!/bin/sh
-docker build . -t domix/obs-util
-echo
-echo
-echo "To run the docker container execute:"
-echo "    $ docker run -p 8080:8080 obs-util"
+#!/usr/bin/env bash
+
+VERSION=0.0.1
+REPO="domix/obs-util"
+
+docker build -t ${REPO}:${VERSION} -t ${REPO}:latest . && \
+   git release $VERSION && \
+   docker push ${REPO}:${VERSION} && \
+   docker push ${REPO}:latest && \
+   echo "Done"
+
+exit_code=$?
+
+docker rmi -f $(docker images -q --filter label=stage=builder)
+
+exit "$exit_code"
