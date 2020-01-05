ac9856e50509744bc0dfe48cf66324669d1a44da
Domingo Suarez Torres <domingo.suarez@gmail.com>
2020-01-05 17:39:59 -0600

    moved to better place the baseDir config

--- src/main/java/obs/util/model/ActiveVideo.java
+++ src/main/java/obs/util/model/ActiveVideo.java
@@ -11,6 +11,7 @@ public class ActiveVideo {
   private Video video = null;
   private Integer resourceIndex = 0;
   private Integer participantIndex = 0;
+  private String baseWorkDir;
 
   public void incResourceIndex() {
     log.info("Resource Index before inc: {}", resourceIndex);
@@ -47,5 +48,65 @@ public class ActiveVideo {
     log.info("Participant Index before reset: {}", participantIndex);
     participantIndex = 0;
     log.info("Participant Index after reset: {}", participantIndex);
+  }
+
+  public String getShowNameFile() {
+    return baseWorkDir + "/showName.txt";
+  }
+
+  public String getShowTitleFile() {
+    return baseWorkDir + "/showTitle.txt";
+  }
+
+  public String getShowSubtitleFile() {
+    return baseWorkDir + "/showSubtitle.txt";
+  }
+
+  public String getActiveResourceTitleFile() {
+    return baseWorkDir + "/resourceTitle.txt";
+  }
+
+  public String getActiveResourceUrlFile() {
+    return baseWorkDir + "/resourceUrl.txt";
+  }
+
+  public String getActiveResourceDescriptionFile() {
+    return baseWorkDir + "/resourceDescription.txt";
+  }
+
+  public String getActiveResourceSummaryFile() {
+    return baseWorkDir + "/resourceSummary.txt";
+  }
+
+  public String getActiveResourceTypeIconFile() {
+    return baseWorkDir + "/resourceTypeIcon.txt";
+  }
+
+  public String getActiveResourceTypeNameFile() {
+    return baseWorkDir + "/resourceTypeName.txt";
+  }
+
+  public String getParticipantRoleFile(Integer index) {
+    return baseWorkDir + "/participant_" + index + "_role.txt";
+  }
+
+  public String getParticipantNameFile(Integer index) {
+    return baseWorkDir + "/participant_" + index + "_name.txt";
+  }
+
+  public String getParticipantTwitterFile(Integer index) {
+    return baseWorkDir + "/participant_" + index + "_twitter.txt";
+  }
+
+  public String getParticipantGitHubFile(Integer index) {
+    return baseWorkDir + "/participant_" + index + "_github.txt";
+  }
+
+  public String getParticipantCompanyFile(Integer index) {
+    return baseWorkDir + "/participant_" + index + "_company.txt";
+  }
+
+  public String getParticipantCompanyTitleFile(Integer index) {
+    return baseWorkDir + "/participant_" + index + "_companyTitle.txt";
   }
 }
