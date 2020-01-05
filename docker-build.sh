4ec16d88a9641766d5332f9395af8608d3ffc161
Domingo Suarez Torres <domingo.suarez@gmail.com>
2020-01-05 00:32:44 -0600

    added GitHub Docker package publication

--- docker-build.sh
+++ docker-build.sh
@@ -7,10 +7,11 @@ VERSION=$(echo "$TAG" | tr '[:upper:]' '[:lower:]')
 
 echo $VERSION
 
-docker build -t ${REPO}:${VERSION} -t ${REPO}:latest . && \
+docker build -t ${REPO}:${VERSION} -t ${REPO}:latest . -t docker.pkg.github.com/${REPO}/obs-util:${VERSION} && \
    git release $VERSION && \
    docker push ${REPO}:${VERSION} && \
    docker push ${REPO}:latest && \
+   docker push docker.pkg.github.com/${REPO}/obs-util:${VERSION} && \
    echo "Done"
 
 exit_code=$?
