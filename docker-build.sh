0c2d463cd57908efc3ba0939231b0d1b5a54ae41
Domingo Suarez Torres <domingo.suarez@gmail.com>
2020-03-27 12:21:00 -0600

    Adds Jib to build OCI image

--- docker-build.sh
+++ docker-build.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
 
-REPO="domix/obs-util"
+REPO=$(grep "repo" gradle.properties|cut -d'=' -f2)
 TAG=$(grep "version" gradle.properties|cut -d'=' -f2)
 VERSION=$(echo "$TAG" | tr '[:upper:]' '[:lower:]')
 
-
 echo $VERSION
 
-docker build -t ${REPO}:${VERSION} -t ${REPO}:latest . -t docker.pkg.github.com/${REPO}/obs-util:${VERSION} && \
-   git release $VERSION && \
-   docker push ${REPO}:${VERSION} && \
-   docker push ${REPO}:latest && \
-   docker push docker.pkg.github.com/${REPO}/obs-util:${VERSION} && \
-   echo "Done"
-
-exit_code=$?
-
-docker rmi -f $(docker images -q --filter label=stage=builder)
-
-exit "$exit_code"
+./gradlew clean jibDockerBuild -x test && \
+  git release VERSION && \
+  docker tag ${REPO}:${VERSION} docker.pkg.github.com/${REPO}/obs-util:${VERSION} && \
+  docker push ${REPO}:${VERSION} && \
+  docker push ${REPO}:latest && \
+  docker push docker.pkg.github.com/${REPO}/obs-util:${VERSION} && \
+  echo "Done"
