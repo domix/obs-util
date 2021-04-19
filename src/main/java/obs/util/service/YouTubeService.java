4f6d2a5f78424165cd6b2b634ece437db9a161b5
Domingo Suarez Torres <domingo.suarez@gmail.com>
2021-04-18 22:53:08 -0500

    try to fix the build

--- src/main/java/obs/util/service/YouTubeService.java
+++ src/main/java/obs/util/service/YouTubeService.java
@@ -7,7 +7,8 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
+
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.*;
@@ -41,7 +42,7 @@ import static java.util.stream.Collectors.toList;
 @Singleton
 public class YouTubeService {
   private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
-  private static final JsonFactory JSON_FACTORY = new JacksonFactory();
+  private static final JsonFactory JSON_FACTORY = new GsonFactory();
   public static final String APPLICATION_NAME = "obs-util";
   private  VideosService videosService;
   private final TaskScheduler taskScheduler;
